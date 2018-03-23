bd41ee89a118c319bb17b3db622fed55b2b7d9c0
SavageLoop <32804644+SavageLoop@users.noreply.github.com>
2018-03-23 23:32:17 +0100

    Create Skeleton.cpp

---
+++ Class/Source/Skeleton.cpp
@@ -0,0 +1,12 @@
+#include "Skeleton.h"
+
+
+
+Skeleton::Skeleton()
+{
+	creatureHp = 100;
+	creatureDodgeRate = 10; //szansa na dodge jeden pies i kot
+	creatureStrengh = 10;
+	creatureDefence = 2;
+}
+

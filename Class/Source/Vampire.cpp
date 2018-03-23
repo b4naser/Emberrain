f01edaffdd0459fefb71ae229da8cfa2054a5cf8
SavageLoop <32804644+SavageLoop@users.noreply.github.com>
2018-03-23 23:29:11 +0100

    Create Vampire.cpp

---
+++ Class/Source/Vampire.cpp
@@ -0,0 +1,24 @@
+#include "Vampire.h"
+
+
+
+Vampire::Vampire()
+{
+	creatureHp = 120;
+	creatureDodgeRate = 5; //dodge
+	creatureStrengh = 12; //sila uderzenia
+	creatureDefence = 2; //obrona
+	LifeSteal = 5;  // wampiryzm
+}
+
+bool Vampire::CallOfBlood() //funckja przywracajaca zdrowie wampirowi (równość ataku vampira)
+{
+	if ((std::rand() % 100) < LifeSteal) return true;
+	else							return false;
+	return false;
+}
+
+
+Vampire::~Vampire()
+{
+}

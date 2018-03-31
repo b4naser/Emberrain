1ac2bf48b6cb5bbd5c1e653f0db870820ed3cb3c
SavageLoop <32804644+SavageLoop@users.noreply.github.com>
2018-03-31 15:39:56 +0200

    Update Minotaur.cpp

--- Class/Source/Minotaur.cpp
+++ Class/Source/Minotaur.cpp
@@ -4,21 +4,15 @@
 
 Minotaur::Minotaur()
 {
+	creatureName = "Minotaur";
 	creatureHp = 140;
+	LifeSteal = 0;
 	creatureDodgeRate = 5; //dodge
 	creatureStrengh = 16; //sila uderzenia
 	creatureDefence = 4; //obrona
-	stun = 5;  //szansa na stuna
-}
-
-bool Minotaur::minotaurStun() 
-{
-		if ((std::rand() % 100) <= stun) return true;
-		else							return false;
+	minotaurStunRate = 5;  //szansa na stuna
 }
 
 Minotaur::~Minotaur()
 {
 }
-
-

9d65fab21478943de7871618a4495389c8976cb8
SavageLoop <32804644+SavageLoop@users.noreply.github.com>
2018-03-31 15:38:49 +0200

    Update Enemy.cpp

--- Class/Source/Enemy.cpp
+++ Class/Source/Enemy.cpp
@@ -2,17 +2,29 @@
 
 void Enemy::creatureHpDicrease(int damageDone)
 {
-	creatureHp -= (damageDone-creatureDefence);
+	creatureHp = creatureHp - (damageDone-creatureDefence);
 }
 
-
-
-int Enemy::getCreatureHp() const //zwraca hp
+int Enemy::getCreatureHp() const 
 {
 	return creatureHp;
 }
 
- bool Enemy::creatureDodge() const //zwraca ilosc uniku procentowo
+int Enemy::getCreatureStrengh() const
+{
+	return creatureStrengh;
+}
+
+int Enemy::getCreatureDodgeRate() const
+{
+	return creatureDodgeRate;
+}
+
+
+
+
+
+ bool Enemy::creatureDodge() const //zwraca ilosc uniku
 {
 	 int temp = (std::rand() % 100);
 		 if (temp <= creatureDodgeRate)
@@ -20,9 +32,62 @@ int Enemy::getCreatureHp() const //zwraca hp
 	 return false;
 }
 
-int Enemy::creatureAttack() const
+int Enemy::creatureAttack()
 {
 	int attack = ( (std::rand() % ((creatureStrengh/2)+1) ) + (creatureStrengh / 2) );
+	creatureTurnAttackValue = attack;
 	return attack;
+}
+
+std::string Enemy::getCreatureName() const
+{
+	return creatureName;
+}
+
+int Enemy::getCreatureTurnAttackValue() const
+{
+	return creatureTurnAttackValue;
+}
+
+int Enemy::getCreatureDefence() const
+{
+	return creatureDefence;
+}
+bool Enemy::vampireBite() 
+{
+	if ((std::rand() % 100) <= LifeSteal) return true;
+	else						        	return false;
+	return false;
+}
+
+void Enemy::lifeStealing(int def)
+{
+	if (creatureHp != 120) {
+		creatureHp += (getCreatureTurnAttackValue()-def);
+		if (creatureHp > 120) creatureHp = 120;
+	}
+		
+
+}
+
+int Enemy::creatureLifeStealRate()
+{
+	return LifeSteal;
+}
+bool Enemy::minotaurStun()  
+{
+	if ((std::rand() % 100) <= minotaurStunRate) return true;
+	else										 return false;
+}
+
+Enemy::Enemy()
+{
+}
+
+
+
+int Enemy::getMinotaurStunRate()
+{
+	return minotaurStunRate;
 }
 
